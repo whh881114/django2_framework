c4cd508d21e49f5345bf5c4cc85ffa98bca0da81
Roy Wong <whh881114@gmail.com>
2019-06-26 09:29:41 +0800

    在Hello_V2中添加打印记录，会在uwsgi定义的日志文件中打印出来。

--- apps/app_demo1/views/v_hello_v2.py
+++ apps/app_demo1/views/v_hello_v2.py
@@ -9,6 +9,8 @@ from django.http import JsonResponse
 
 class Hello_V2(generics.ListCreateAPIView):
     def get(self, request):
+        # 测试打印内容，会打印到uwsgi的日格文件中。
+        # print('This is a test.')
         ret = {
             "status": "True",
             "code": 0,
